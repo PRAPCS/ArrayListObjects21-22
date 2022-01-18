b07611ff3b6127dac39edf4b80374260528b1a77
Mr. Burger <32077861+PRAPCS@users.noreply.github.com>
2022-01-18 19:05:16 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 import java.util.*;
 class Main {
   public static void main(String[] args) {
-    
+    //Creates an ArrayList called garage that stores car objects
     ArrayList<Cars> garage = new ArrayList<Cars>();
 
+    //Creates a Car Object
     Cars car1 = new Cars("Red",32);
+    //Adds the Car object to the list
     garage.add(car1);
-
+    //Creates and adds a car object to the list all in one line
     garage.add(new Cars("Blue",42));
-
+    //Cycles through the list getting each object one at a time and runs the toString() method
     for(int i=0; i<garage.size();i++){
       System.out.println(garage.get(i));
     }
